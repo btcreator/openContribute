69e71ee95faa1b340ee9e214357bf338bf82eda3
Peter Duricek <p.duricek@gmx.de>
2024-09-04 16:57:21 +0200

    Require destructuring assignment

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { checkId } = require('./../utils/helpers');
-const authController = require('./../controller/authentication/authController');
+const { authenticate, restrictedTo } = require('./../controller/authentication/authController');
 const projectController = require('../controller/projectController');
 
 const router = express.Router();
@@ -9,7 +9,7 @@ router.route('/search').get(projectController.getSearchResults);
 router.route('/:id').get(checkId, projectController.getProject);
 
 // After this point an authentication is needed
-router.use(authController.authenticate);
+router.use(authenticate);
 
 router.route('/myProjects').get(projectController.myProjects).post(projectController.createMyProject);
 router
@@ -19,7 +19,7 @@ router
   .delete(projectController.deleteMyProject);
 
 // After this point is the restricted area for admins
-router.use(authController.restrictedTo('admin'));
+router.use(restrictedTo('admin'));
 
 router.route('/').get(projectController.getAllProjects).post(projectController.createProject);
 router.route('/:id').all(checkId).patch(projectController.updateProject).delete(projectController.deleteProject);
