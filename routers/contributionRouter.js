3b17ae67fa1dd40e6365e34934e4fcfc2cb39ce4
Peter Duricek <p.duricek@gmx.de>
2024-09-20 21:11:25 +0200

    confusing name conti changed to contri

--- routers/contributionRouter.js
+++ routers/contributionRouter.js
@@ -1,45 +1,45 @@
 const express = require('express');
 const { authenticate, restrictedTo } = require('./../controller/authentication/authController');
-const contiController = require('./../controller/contributionController');
+const contriController = require('./../controller/contributionController');
 const { checkId } = require('../utils/helpers');
 
 const router = express.Router();
 
 // Public routes
-router.route('/stat').get(contiController.getStats);
+router.route('/stat').get(contriController.getStats);
 
 // For guests / public
 router
   .route('/myContribution')
-  .all(contiController.isGuest)
-  .get(contiController.getGuestContribution)
-  .post(contiController.createGuestContribution)
-  .patch(contiController.updateGuestContribution)
-  .delete(contiController.deleteGuestContribution);
+  .all(contriController.isGuest)
+  .get(contriController.getGuestContribution)
+  .post(contriController.createGuestContribution)
+  .patch(contriController.updateGuestContribution)
+  .delete(contriController.deleteGuestContribution);
 
 // After this point an authentication is needed
 router.use(authenticate);
 
-router.route('/myContributions').get(contiController.myContributions);
-router.route('/myContribution').post(contiController.createMyContribution);
+router.route('/myContributions').get(contriController.myContributions);
+router.route('/myContribution').post(contriController.createMyContribution);
 
 router
   .route('/myContribution/:id')
   .all(checkId)
-  .get(contiController.getMyContribution)
-  .patch(contiController.updateMyContribution)
-  .delete(contiController.deleteMyContribution);
+  .get(contriController.getMyContribution)
+  .patch(contriController.updateMyContribution)
+  .delete(contriController.deleteMyContribution);
 
 // After this point is the restricted area for admins
 router.use(restrictedTo('admin'));
 
-router.route('/').get(contiController.getAllContributions).post(contiController.createContribution);
+router.route('/').get(contriController.getAllContributions).post(contriController.createContribution);
 
 router
   .route('/:id')
   .all(checkId)
-  .get(contiController.getContribution)
-  .patch(contiController.updateContribution)
-  .delete(contiController.deleteContribution);
+  .get(contriController.getContribution)
+  .patch(contriController.updateContribution)
+  .delete(contriController.deleteContribution);
 
 module.exports = router;
