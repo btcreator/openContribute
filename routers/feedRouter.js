b8c66eb46a5e3f4a984638efc16f28b5acad5ee2
Peter Duricek <p.duricek@gmx.de>
2024-10-09 11:15:13 +0200

    Initial commit

---
+++ routers/feedRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { checkId } = require('./../utils/helpers');
+const { identificateUser, authenticate, restrictedTo } = require('./../controller/authentication/authController');
+const feedController = require('./../controller/feedController');
+
+const router = express.Router();
+
+router.route('/project/:id').get(checkId, identificateUser, feedController.getProjectsFeed);
+
+router.use(authenticate);
+router.route('/myFeed').post(feedController.createMyFeed);
+router.route('/myFeed/:id').patch(feedController.updateMyFeed).delete(feedController.deleteMyFeed);
+
+router.use(restrictedTo('admin'));
+router.route('/').post(feedController.createFeed).get(feedController.getAllFeed);
+router.route('/:id').patch(feedController.updateFeed).delete(feedController.deleteFeed);
+
+module.exports = router;
