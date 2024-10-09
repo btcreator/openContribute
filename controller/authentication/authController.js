e5f168e665274e77dddac502bccdfac23c2bdf08
Peter Duricek <p.duricek@gmx.de>
2024-10-09 11:04:09 +0200

    Set cookies to new-third-party-cookies-policy ready

--- controller/authentication/authController.js
+++ controller/authentication/authController.js
@@ -13,6 +13,8 @@ const _cookieOptions = {
   expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
   httpOnly: true,
   secure: true,
+  sameSite: 'none',
+  partitioned: true,
 };
 
 // Token related functions
