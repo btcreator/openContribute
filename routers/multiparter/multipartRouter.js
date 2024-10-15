ba0cd9640582be806c79d8543a5ec52782c31396
Peter Duricek <p.duricek@gmx.de>
2024-10-15 18:51:13 +0200

    User has now an object field, so need reparse the body when multipart data is sent

--- routers/multiparter/multipartRouter.js
+++ routers/multiparter/multipartRouter.js
@@ -14,7 +14,13 @@ const router = express.Router();
 
 router
   .route(/.*user.*/)
-  .all(passMethods('patch', 'post'), checkContetType, bufferImage('userPhoto'), editAndSaveUserImage);
+  .all(
+    passMethods('patch', 'post'),
+    checkContetType,
+    bufferImage('userPhoto'),
+    editAndSaveUserImage,
+    reparseMultipartBody
+  );
 router.route(/.*project.*/).all(passMethods('patch', 'post'), checkContetType, saveProjectImages, reparseMultipartBody);
 router.route(/.*contribution.*/).all(passMethods('patch', 'post'), checkContetType, parseTextOnlyMultipartBody());
 router.route(/.*feed.*/).all(passMethods('patch', 'post'), checkContetType, uploadFeedMultimedia);
