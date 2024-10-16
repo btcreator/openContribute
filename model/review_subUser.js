a755af783035359cf6766ecf7c0b3363eaf4c7e8
Peter Duricek <p.duricek@gmx.de>
2024-10-16 20:20:09 +0200

    Initial commit

---
+++ model/review_subUser.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    _id: false,
+    review: {
+      type: String,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: false,
+    },
+  }
+);
+
+reviewSchema.pre('validate', function (next) {
+  if (this.review === '') this.review = undefined;
+  next();
+});
+
+module.exports = reviewSchema;
