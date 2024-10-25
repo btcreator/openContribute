2088dfb24d7a0f3259e9e2eadecb767ad0cf158b
Peter Duricek <p.duricek@gmx.de>
2024-10-25 11:09:03 +0200

    Added new pipeline to count all contributors

--- controller/aggregationPipelines/statsPipelines.js
+++ controller/aggregationPipelines/statsPipelines.js
@@ -1,71 +1,52 @@
 // Return the stats of the projects which are done or open
-exports.projectStatsPipeline = () => [
+exports.projectStatsPipeline = [
   // stage 1 - each document gets controlled
   {
     $match: {},
   },
-  // stage 2 - grouping in 4 docs - done and active(removed or not) are booleans
+  // stage 2 - grouping based on leaders, and how many open and done projects they have
   {
     $group: {
-      _id: {
-        done: '$isDone',
-        active: '$isActive',
-      },
-      count: {
-        $sum: 1,
-      },
-    },
-  },
-  // stage 3 - grouping all done projects and all active undone projects
-  {
-    $group: {
-      _id: {
-        $cond: {
-          if: { $eq: ['$_id.done', true] },
-          then: 'done',
-          else: {
-            $cond: {
-              if: { $eq: ['$_id.active', true] },
-              then: 'open',
-              else: null,
-            },
+      _id: '$leader',
+      done: {
+        $sum: {
+          $cond: {
+            if: '$isDone',
+            then: 1,
+            else: 0,
           },
         },
       },
-      count: { $sum: '$count' },
-    },
-  },
-  // stage 4 - undone and inactive projects are ignored (projects was removed before done)
-  {
-    $match: {
-      _id: { $ne: null },
-    },
-  },
-  // stage 5 - prepare for convert to object
-  {
-    $group: {
-      _id: null,
-      proj: {
-        $push: {
-          k: '$_id',
-          v: '$count',
+      open: {
+        $sum: {
+          $cond: {
+            if: { $and: [{ $not: '$isDone' }, '$isActive'] },
+            then: 1,
+            else: 0,
+          },
         },
       },
     },
   },
-  // stage 6 - convert result to object
+  // stage 3 - count leaders, and sum they done/open projects
+  {
+    $group: {
+      _id: 'null',
+      leaders: { $sum: 1 },
+      done: { $sum: '$done' },
+      open: { $sum: '$open' },
+    },
+  },
+  // stage 4 - remove unnecessary field
   {
     $project: {
       _id: 0,
-      proj: {
-        $arrayToObject: '$proj',
-      },
     },
   },
 ];
 
 // Return the stats of the resources how much was contributed to them
-exports.resourceStatsPipeline = () => [
+exports.resourceStatsPipeline = [
   // stage 1 - each document gets controlled
   {
     $match: {},
@@ -99,3 +80,113 @@ exports.resourceStatsPipeline = () => [
     },
   },
 ];
+
+exports.contributorsStatPipeline = [
+  // stage 1 - each document gets controlled
+  {
+    $match: {},
+  },
+  // stage 2 - grouping based on user aka. contributor - guests are counted too (null)
+  {
+    $group: {
+      _id: '$user',
+    },
+  },
+  // stage 3 - group them together and count the documents, i.e. all unique users (with guests)
+  {
+    $group: {
+      _id: null,
+      contributors: {
+        $sum: 1,
+      },
+    },
+  },
+  // stage 4 - project fields
+  {
+    $project: {
+      _id: 0,
+      contributors: 1,
+    },
+  },
+];
+
+/*
+resourceStatsPipeline and contributorsStatPipeleine could be merged together and get with one call,
+but it is less performant (more memory usage, and more burden for mongo server because of multiple group and unwind operations)
+[{
+    $match: {}
+  },
+  {
+    $group: {
+      _id: {
+        resource: "$resource",
+        user: "$user"
+      },
+      count: {
+        $sum: "$amount"
+      }
+    }
+  },
+  {
+    $group: {
+      _id: "$_id.user",
+      resourceArr: {
+        $push: {
+          res: "$_id.resource",
+          count: "$count"
+        }
+      }
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      users: {
+        $sum: 1
+      },
+      resources: {
+        "$push": "$resourceArr"
+      }
+    }
+  },
+  {
+    "$unwind": "$resources"
+  },
+  {
+    $unwind: "$resources"
+  },
+  {
+    $group: {
+      _id: "$resources.res",
+      count: {
+        $sum: "$resources.count"
+      },
+      contributors: {
+        "$first": "$users"
+      }
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      resources: {
+        $push: {
+          k: "$_id",
+          v: "$count"
+        }
+      },
+      contributors: {
+        $first: "$contributors"
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      contributors: 1,
+      resources: {
+        $arrayToObject: "$resources"
+      }
+    }
+  }
+] */
