aeae3c349f7b483cb3a663a762ec45083d7cebe8
Peter Duricek <p.duricek@gmx.de>
2024-11-03 17:54:06 +0100

    Changed creation time to automatic by mongoose

--- model/feed.js
+++ model/feed.js
@@ -1,52 +1,56 @@
 const mongoose = require('mongoose');
 const { removeFiles } = require('./../controller/staticFilesystem/staticFileController');
 
-const feedSchema = new mongoose.Schema({
-  project: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Project',
-    required: true,
-    select: false,
-  },
-  leader: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  message: {
-    type: String,
-    trim: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  isMilestone: {
-    type: Boolean,
-    default: false,
-  },
-  images: [String],
-  videos: [String],
-  link: {
-    type: String,
-    validate: {
-      validator: function (link) {
-        const reg = /(?<=youtube.com\/watch\?v=)[^&]+/;
-        const match = reg.exec(link);
-        if (!match) return false;
+const feedSchema = new mongoose.Schema(
+  {
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Project',
+      required: true,
+      select: false,
+    },
+    leader: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      trim: true,
+    },
+    isMilestone: {
+      type: Boolean,
+      default: false,
+    },
+    images: [String],
+    videos: [String],
+    link: {
+      type: String,
+      validate: {
+        validator: function (link) {
+          const reg = /(?<=youtube.com\/watch\?v=)[^&]+/;
+          const match = reg.exec(link);
+          if (!match) return false;
 
-        this.set('link', `https://www.youtube.com/watch?v=${match[0]}`);
-        return true;
+          this.set('link', `https://www.youtube.com/watch?v=${match[0]}`);
+          return true;
+        },
+        message: 'Link provided is not a vlaid youtube link.',
       },
-      message: 'Link provided is not a vlaid youtube link.',
     },
   },
-});
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: false,
+    },
+  }
+);
 
 // Indexing
 ////
