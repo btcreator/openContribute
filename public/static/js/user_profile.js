7d388bb71a972f77aa542875e09d6808e8b6c148
Peter Duricek <p.duricek@gmx.de>
2024-11-09 21:10:16 +0100

    User profile photo update functionality added.

--- public/static/js/user_profile.js
+++ public/static/js/user_profile.js
@@ -2,8 +2,12 @@ import { updateProfile, passwordChange } from './_profile_api_calls.js';
 import { setAlert } from './_alert.js';
 
 const btnChangePhoto = document.querySelector('.change-photo');
+const userImage = document.querySelector('.photo-frame img');
+const userNavImg = document.querySelector('.profile img');
 const formSaveProfile = document.querySelector('.profile-form');
 const formPasswordChange = document.querySelector('.password-change-form');
+const fileInput = document.createElement('input');
+fileInput.setAttribute('type', 'file');
 
 formSaveProfile.addEventListener('submit', async function (ev) {
   ev.preventDefault();
@@ -34,4 +38,17 @@ formPasswordChange.addEventListener('submit', async function (ev) {
   });
 
   if (res?.status === 200) window.location.replace('/login/?alert=Please Log in with your new password.');
+});
+
+// button for change photo is just a pseudo button for file input
+btnChangePhoto.addEventListener('click', () => fileInput.click());
+fileInput.addEventListener('change', async function (ev) {
+  const form = new FormData();
+  form.append('userPhoto', ev.target.files[0]);
+
+  const res = await updateProfile(form);
+  if (res?.status === 200) {
+    setAlert('Profile photo updated.');
+    userImage.src = userNavImg.src = `/media/users/${res.data.data.user.photo}?t=${new Date().getTime()}`;
+  }
 });
