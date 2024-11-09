45c3d878fd02ec2b5a9113154d22ba1a5773480e
Peter Duricek <p.duricek@gmx.de>
2024-11-09 19:05:52 +0100

    Redirec user after login or signup to homepage

--- public/static/js/signIn.js
+++ public/static/js/signIn.js
@@ -1,5 +1,5 @@
-import { login, signup } from './_signIn_api_calls';
-import { toggleSignInWindowInit } from './_signInToggleWindow';
+import { login, signup } from './_signIn_api_calls.js';
+import { toggleSignInWindowInit } from './_signInToggleWindow.js';
 
 const loginForm = document.querySelector('#login-form');
 const signupForm = document.querySelector('#signup-form');
@@ -8,20 +8,22 @@ const signupForm = document.querySelector('#signup-form');
 toggleSignInWindowInit();
 
 // log in
-loginForm.addEventListener('submit', (ev) => {
+loginForm.addEventListener('submit', async (ev) => {
   ev.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  login(email, password);
+  const res = await login(email, password);
+  if (res?.status === 200) window.location.replace('/?alert="You logged In Successfully"');
 });
 
 // Sign up
-signupForm.addEventListener('submit', (ev) => {
+signupForm.addEventListener('submit', async (ev) => {
   ev.preventDefault();
   const email = document.getElementById('email-sign');
   const password = document.getElementById('password-sign');
   const confirmPassword = document.getElementById('confirm-password');
 
-  signup(email, password, confirmPassword);
+  const res = await signup(email, password, confirmPassword);
+  if (res?.status === 204) window.location.replace('/?alert="Welcome on board!"');
 });
