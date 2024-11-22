4f9b0aea0264e46309bdd7b88ba4b3e6c6ae265c
Peter Duricek <p.duricek@gmx.de>
2024-11-22 20:54:13 +0100

    Security policy updated for youtube thumbnails to projects feed

--- security.js
+++ security.js
@@ -20,7 +20,7 @@ router.use(
   helmet.contentSecurityPolicy({
     directives: {
       'script-src': ['unpkg.com', 'https://www.youtube.com/', 'http://localhost:3000'],
-      'img-src': ['https://tile.openstreetmap.org', 'http://localhost:3000'],
+      'img-src': ['https://tile.openstreetmap.org', 'https://img.youtube.com', 'http://localhost:3000'],
       'frame-src': ['https://www.youtube.com/'],
     },
   })
