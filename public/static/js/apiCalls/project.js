4da8fc9bb5db5b6d149f41fb29205931d45b39a7
Peter Duricek <p.duricek@gmx.de>
2025-02-01 13:38:55 +0100

    Find/filter projects for search functionality

--- public/static/js/apiCalls/project.js
+++ public/static/js/apiCalls/project.js
@@ -1,10 +1,17 @@
-import showError from "./../utils/showResponseError.js"
+import showError from './../utils/showResponseError.js';
 
 export const getProject = async function (id) {
-  const project = await axios.get(`/api/v1/project/${id}`).catch(showError);
-  return project?.data.data.project;
+  const response = await axios.get(`/api/v1/project/${id}`).catch(showError);
+  return response?.data.data.project;
 };
 
 export const updateProject = function (id, updateObj) {
   return axios.patch(`/api/v1/project/myProject/${id}`, updateObj).catch(showError);
+};
+
+export const findProjects = function (searchText, filterOptions) {
+  let filterStr = '';
+  for (const [key, val] of Object.entries(filterOptions)) filterStr += `&${key}=${val}`;
+
+  return axios.get(`/api/v1/project/search?name=${searchText}${filterStr}`).catch(showError);
 };
