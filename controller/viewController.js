09350b64428a339ac8edd83bfaf045f6c4b1ffa8
Peter Duricek <p.duricek@gmx.de>
2025-02-01 13:34:56 +0100

    Search functionality implemented + nicering

--- controller/viewController.js
+++ controller/viewController.js
@@ -6,6 +6,7 @@ const { populateContributionsToProjectPipeline } = require('./aggregationPipelin
 const stat = require('./aggregationPipelines/statsPipelines');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('../utils/appError');
+const RefineQuery = require('../utils/refineQuery');
 
 // Alert handler middleware. Passes alert messages to the pug templates
 exports.alertMsgHandler = (req, res, next) => {
@@ -19,6 +20,7 @@ exports.alertMsgHandler = (req, res, next) => {
   next();
 };
 
+// Home page
 exports.getHome = catchAsync(async function (req, res) {
   const projects = (await Project.aggregate(stat.projectStatsPipeline))[0];
   const resList = await Contribution.aggregate(stat.resourceStatsPipeline);
@@ -49,6 +51,7 @@ exports.getHome = catchAsync(async function (req, res) {
   });
 });
 
+// Login page
 exports.login = (req, res) => {
   res.status(200).render('signIn', {
     title: 'User Login / Signup',
@@ -56,6 +59,59 @@ exports.login = (req, res) => {
   });
 };
 
+// Search page
+exports.search = catchAsync(async (req, res) => {
+  const selector = 'name slug leader summary type resources resultImg isDone';
+  const projectsQuery = Project.find().populate('leader', 'name -_id');
+
+  // search with url query when the search from other page was hit
+  const searchText = req.query?.name ?? "";
+  const query = new RefineQuery(projectsQuery, { limit: 10, sort: 'createdAt', isDone: false }).refine({ isActive: true }, selector);
+  const projects = await query.where('name', new RegExp(searchText, 'i'));
+
+  res.status(200).render('search', {
+    title: 'Search for project',
+    user: req.user,
+    projects,
+  });
+});
+
+// User page
+////
+// profile details
+exports.myProfile = (req, res) => {
+  res.status(200).render('user_profile', {
+    title: 'My Profile',
+    user: req.user,
+  });
+};
+
+// contributions 
+exports.myContributions = catchAsync(async (req, res) => {
+  const contributions = await Contribution.aggregate(summaryPipeline(req.user._id));
+
+  res.status(200).render('user_contributions', {
+    title: 'My Contributions',
+    user: req.user,
+    contributions,
+  });
+});
+
+// projects
+exports.myProjects = catchAsync(async (req, res) => {
+  const projects = await Project.find({ leader: req.user._id, isActive: true }).select(
+    'name resultImg progress isDone slug'
+  );
+
+  res.status(200).render('user_projects', {
+    user: req.user,
+    projects,
+  });
+});
+
+// Project page
+////
+// show
 exports.showProject = catchAsync(async (req, res) => {
   const project = await Project.aggregate(
     populateContributionsToProjectPipeline({ slug: req.params.slug, isActive: true })
@@ -74,34 +130,7 @@ exports.showProject = catchAsync(async (req, res) => {
   });
 });
 
-exports.myProfile = (req, res) => {
-  res.status(200).render('user_profile', {
-    title: 'My Profile',
-    user: req.user,
-  });
-};
-
-exports.myContributions = catchAsync(async (req, res) => {
-  const contributions = await Contribution.aggregate(summaryPipeline(req.user._id));
-
-  res.status(200).render('user_contributions', {
-    title: 'My Contributions',
-    user: req.user,
-    contributions,
-  });
-});
-
-exports.myProjects = catchAsync(async (req, res) => {
-  const projects = await Project.find({ leader: req.user._id, isActive: true }).select(
-    'name resultImg progress isDone slug'
-  );
-
-  res.status(200).render('user_projects', {
-    user: req.user,
-    projects,
-  });
-});
-
+// update
 exports.updateMyProject = catchAsync(async (req, res) => {
   const project = await Project.aggregate(
     populateContributionsToProjectPipeline({ slug: req.params.slug, leader: req.user._id, isActive: true })
