840f7de005242b4a1057a2b192e95620b1a45af1
Peter Duricek <p.duricek@gmx.de>
2025-02-07 17:40:21 +0100

    Search input functionality added

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,18 +1,21 @@
 const heroText = document.querySelector('.tail-text');
+const inputSearch = document.getElementById("search");
+const btnSearch = document.querySelector('.btn-search');
 
-if(heroText) {
-  // Hero text ease-in / -out
-  const changeText = () => {
-    const tails = ['ceive', 'nect', 'struct', 'tribute'];
-    let counter = 1;
-    return () => {
-      heroText.classList.add('reverse');
-      setTimeout(() => {
-        heroText.textContent = tails[counter++];
-        heroText.classList.remove('reverse');
-      }, 1300);
-      if (counter >= tails.length) counter = 0;
-    };
+// Hero text ease-in / -out
+const changeText = () => {
+  const tails = ['ceive', 'nect', 'struct', 'tribute'];
+  let counter = 1;
+  return () => {
+    heroText.classList.add('reverse');
+    setTimeout(() => {
+      heroText.textContent = tails[counter++];
+      heroText.classList.remove('reverse');
+    }, 1300);
+    if (counter >= tails.length) counter = 0;
   };
-  setInterval(changeText(), 4500);
-}
+};
+setInterval(changeText(), 4500);
+
+// Search for projects with given name
+btnSearch.addEventListener("click", ()=> window.location.replace(`/search?q=${inputSearch.value}`));
