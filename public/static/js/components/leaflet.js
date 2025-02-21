355363b41bd612709144156e3e71fb5b09623367
Peter Duricek <p.duricek@gmx.de>
2025-02-21 21:37:05 +0100

    Leaflet map controller is more robust for multiple use - marker can be set/reset and send the clicked position on the map to an output/reset the marker

--- public/static/js/components/leaflet.js
+++ public/static/js/components/leaflet.js
@@ -1,13 +1,21 @@
-export const displayMap = function (locations) {
-  // set initial map settings
-  const map = L.map('map', {
-    attributionControl: false,
-    zoomControl: false,
-    scrollWheelZoom: false,
-    doubleClickZoom: true,
-    zoomSnap: 0.1,
-  });
+// Set marker icon
+const icon = L.icon({
+  iconUrl: '/static/img/icons/pin.png',
+  iconSize: [20, 25],
+  iconAnchor: [10, 25],
+  popupAnchor: [0, -25],
+});
 
+// Set initial map settings
+const map = L.map('map', {
+  attributionControl: false,
+  zoomControl: false,
+  scrollWheelZoom: false,
+  doubleClickZoom: true,
+  zoomSnap: 0.1,
+});
+
+export const displayMap = function (locations) {
   // add layer
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -16,22 +24,49 @@ export const displayMap = function (locations) {
   // add controls
   L.control.zoom({ position: 'topright' }).addTo(map);
 
-  // set bounds and marker icon
-  const bounds = L.latLngBounds();
-  const icon = L.icon({
-    iconUrl: '/static/img/icons/pin.png',
-    iconSize: [20, 25],
-    iconAnchor: [10, 25],
-    popupAnchor: [0, -25],
-  });
- 
-  // add to each location a marker and push bounds
-  locations.forEach((loc) => {
-    const coord = loc.coordinates.reverse();
-    bounds.extend(coord);
-    L.marker(coord, { icon }).bindPopup(`<p>${loc.name}</p>`).addTo(map);
-  });
+  if (locations?.length) {
+    // set bounds
+    const bounds = L.latLngBounds();
 
-  // set map to boundaries
-  map.fitBounds(bounds.pad(30));
+    // add to each location a marker and push bounds
+    locations.forEach((loc) => {
+      const coord = loc.coordinates.reverse();
+      bounds.extend(coord);
+      L.marker(coord, { icon }).bindPopup(`<p>${loc.name}</p>`).addTo(map);
+    });
+
+    // set map to boundaries
+    map.fitBounds(bounds.pad(30));
+  } else
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        const coord = pos.coords;
+        map.setView([coord.latitude, coord.longitude], 8);
+      },
+      () => {
+        map.setView([51.505, -0.09], 8);
+      }
+    );
 };
+
+// One marker can be added and reset the same marker (when the user select a position with click on map)
+const _setTheMarker = (() => {
+  const marker = L.marker([0, 0], { icon });
+  let setToMap = false;
+  return function (coord) {
+    marker.setLatLng(coord);
+    setToMap || ((setToMap = !setToMap), marker.addTo(map));
+    map.panTo(coord);
+  };
+})();
+
+export const setTheMarker = _setTheMarker;
+
+// Send the clicked position (lat / lng) to the output
+export const setClickOnMapToOutput = (output) =>
+  map.on('click', (ev) => {
+    if (!ev.originalEvent.ctrlKey) return;
+    const coord = [ev.latlng.lat, ev.latlng.lng];
+    output.value = coord.join();
+    _setTheMarker(coord);
+  });
