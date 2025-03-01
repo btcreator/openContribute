b334eeaad46715b2ec6d95906bd4696f4f905eaf
Peter Duricek <p.duricek@gmx.de>
2025-03-01 12:52:14 +0100

    Each project needs at least one milestone, resource and location

--- model/project.js
+++ model/project.js
@@ -15,6 +15,9 @@ const _uniqueName = function (val) {
   }
 };
 
+// Arrays required validator checks if array is not empty
+mongoose.Schema.Types.Array.checkRequired(v => Array.isArray(v) && v.length);
+
 const projectSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
@@ -51,6 +54,7 @@ const projectSchema = new mongoose.Schema(
           _id: false,
         },
       ],
+      required: [true, "Project needs at least one location."],
       validate: [_uniqueName, 'Duplicate locations are not allowed.'],
     },
     milestones: {
@@ -62,6 +66,7 @@ const projectSchema = new mongoose.Schema(
           _id: false,
         },
       ],
+      required: [true, "Project needs at least one milestone."],
       validate: [_uniqueName, 'Duplicate milestones are not allowed.'],
     },
     resources: {
@@ -86,6 +91,7 @@ const projectSchema = new mongoose.Schema(
           _id: false,
         },
       ],
+      required: [true, "Project needs at least one resource."],
       validate: [_uniqueName, 'Duplicate resources are not allowed.'],
     },
     progress: { type: Number, default: 0, min: 0, max: 1 },
