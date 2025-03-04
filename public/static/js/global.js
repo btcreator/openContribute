d98d3b6587b53a989ce8eebcb0f0d5657196b0d9
Peter Duricek <p.duricek@gmx.de>
2025-03-04 20:23:25 +0100

    Implemented logout and merged with observeAlert to global.js

--- public/static/js/global.js
+++ public/static/js/global.js
@@ -1,6 +1,14 @@
-import { hideAlert } from '../utils/alert.js';
+import { hideAlert } from './utils/alert.js';
+import { logout } from './apiCalls/auth.js';
 
+const btnLogout = document.querySelector('.logout-btn');
 const alertWindow = document.querySelector('.alert-window');
+
+if (btnLogout)
+  btnLogout.addEventListener('click', async (ev) => {
+    const res = await logout();
+    if (res?.status === 200) window.location.replace('/?alert="You logged out successfully!"');
+  });
 
 // When alert window is set to visible on load, it mean it is set by server and there is a message for user to show
 if (!alertWindow.classList.contains('hidden')) {
