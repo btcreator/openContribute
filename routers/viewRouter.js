953b726377b92af1a528c264a29d4d0992e10ff6
Peter Duricek <p.duricek@gmx.de>
2025-03-12 10:42:55 +0100

    Restructured project creation route to control of user information about requirements of project creation

--- routers/viewRouter.js
+++ routers/viewRouter.js
@@ -10,12 +10,12 @@ router.get('/', identificateUser, viewController.getHome);
 router.get('/login', identificateUser, viewController.login);
 router.get('/search', identificateUser, viewController.search);
 router.get('/project/:slug', identificateUser, viewController.showProject);
+router.get('/projectCreate', identificateUser, viewController.createMyProject);
 
 router.use(authenticate);
 router.get('/myProfile', viewController.myProfile);
 router.get('/myContributions', viewController.myContributions);
 router.get('/myProjects', viewController.myProjects);
 router.get('/projectUpdate/:slug', viewController.updateMyProject);
-router.get('/projectCreate', viewController.createMyProject);
 
 module.exports = router;
