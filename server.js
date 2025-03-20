975e35318c05b0bf63217cfaf3883c2fc05b5a06
Peter Duricek <p.duricek@gmx.de>
2025-03-20 21:36:42 +0100

    'Unhandled' error rejections handling added

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 // Load environment variables
 require('@dotenvx/dotenvx').config({ path: './config.env' });
 
+// Error handling of unhandled exceptions (for example catch some bugs)
+process.on('uncaughtException', (err) => {
+  serverLog(`Uncaught exception: ${err}`);
+  process.exit(1);
+});
+
 const mongoose = require('mongoose');
 const app = require('./app');
 const { serverLog } = require('./utils/helpers');
 
 // Establish connection with the database
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-mongoose
-  .connect(DB)
-  .then(() => {
-    serverLog(`Connection to database established...`);
-  })
-  .catch((err) => {
-    serverLog(`Connection to database is terminated...`, err);
-  });
+mongoose.connect(DB).then(() => {
+  serverLog(`Connection to database established...`);
+});
 
 // Start the server
 const port = process.env.PORT || 3500;
 const host = process.env.HOST || 'localhost';
-app.listen(port, host, () => {
+const server = app.listen(port, host, () => {
   serverLog(`Server start up on host:port ${host}:${port}...`);
+});
+
+// Error handling for unhandled rejections (outside of express)
+process.on('unhandledRejection', (err) => {
+  serverLog(`Unhandled rejection: ${err}`);
+  server.close(() => process.exit(1));
 });
