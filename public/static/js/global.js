ca6a7964f91760c509b34d72089951b99040155d
Peter Duricek <p.duricek@gmx.de>
2025-03-26 11:24:18 +0100

    Added Information window about demo page/disclaimer

--- public/static/js/global.js
+++ public/static/js/global.js
@@ -5,6 +5,22 @@ import { logout } from './apiCalls/auth.js';
 
 const btnLogout = document.querySelector('.logout-btn');
 const alertWindow = document.querySelector('.alert-window');
+const demo = document.querySelector('.demo-info');
+const demoInfo = demo.querySelector('p');
+
+// Handle the dropdown modal window
+const modalHandler = ((markup) => {
+  let isOpen = false;
+  const openText = demoInfo.textContent = 'This is just a demo website for testing and learning purpose! Click here.';
+  demo.addEventListener("transitionend", (ev) => isOpen && (demoInfo.innerHTML = markup));
+
+  return (ev) => {
+    isOpen = demo.classList.toggle('modal-open');
+    if(!isOpen) demoInfo.textContent = openText;
+  }
+})(getMarkup());
+
+demo.addEventListener("click", modalHandler);
 
 if (btnLogout)
   btnLogout.addEventListener('click', async (ev) => {
@@ -23,4 +39,18 @@ if (!alertWindow.classList.contains('hidden')) {
     alertWindow.style.opacity = 1;
     hideAlert(3000);
   }, 500);
+}
+
+
+// Dont wanted to put it on the beginnig of the code (because of esthetic :) ) thats why as function
+function getMarkup () {
+  return `Please don't share any personal information!<br/><br/><br/>
+  Disclaimer: </br>
+  By using this demo website, you acknowledge that you are solely responsible for any content you upload or share. I am not responsible for the accuracy, legality, or appropriateness of the content posted. I reserve the right to remove or modify any content at my discretion without prior notice.</br></br>
+  For payments use a testing credit card data: </br>
+  Card Nr.: <b>4242 4242 4242 4242</b> </br>
+  Date: <b>12/34</b> (any date in the future) </br>
+  CVC: <b>any number</b> <br/></br></br>
+  <b>This websites fundamentals is based on the Natours project by <i>Jonas Schmedtmann</i>. On this way want to thanks for his great course!</b> 
+  `;
 }
