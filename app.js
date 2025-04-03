a89246d1556db058cc92385b96b8bcb4c2e022ff
Peter Duricek <p.duricek@gmx.de>
2025-04-03 17:05:58 +0200

    Check proxy / Lb's between user and app

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const app = express();
 
 // For barrier-free connections
 ////
-// if behind proxy - TODO after deploying the app needs check for proxies between client and server
-app.enable('trust proxy');
+// if behind proxy
+// app.enable('trust proxy');
 // enable cross origin resource sharing for everyone
 app.use(cors());
 
