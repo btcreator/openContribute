0c394af3a5849d8a852fef0b94fbb26b94cf6af2
Peter Duricek <p.duricek@gmx.de>
2025-04-21 18:44:36 +0200

    Bug fix - the putput had not the right form.

--- controller/statisticsController.js
+++ controller/statisticsController.js
@@ -10,20 +10,20 @@ const {
 // Basic stats of the projects
 exports.stats = catchAsync(async (req, res) => {
   // projects stats aggregation - about open / already done projects and amount of leaders
-  const projects = await Project.aggregate(projectStatsPipeline);
+  const projects = (await Project.aggregate(projectStatsPipeline))[0] ?? {};
   const leaders = projects.leaders;
   delete projects.leaders;
 
   // resource stats aggregation - about how many of each resources was contributed
-  const resList = await Contribution.aggregate(resourceStatsPipeline);
+  const resources = (await Contribution.aggregate(resourceStatsPipeline))[0]?.resources ?? {};
 
   // contributors stat aggregation - about how many people contributed to all projects
-  const contributors = await Contribution.aggregate(contributorsStatPipeline);
+  const contributors = (await Contribution.aggregate(contributorsStatPipeline))[0]?.contributors;
 
   // the output as stat
   const stats = {
-    projects: projects[0],
-    resources: resList[0].resources,
+    projects,
+    resources,
     members: {
       contributors,
       leaders,
