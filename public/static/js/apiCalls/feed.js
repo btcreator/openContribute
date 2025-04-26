fbdfff1958ee382270418d32c24839652f732445
btcreator <84269672+btcreator@users.noreply.github.com>
2025-04-26 18:32:44 +0200

    Correct the changes response property name

--- public/static/js/apiCalls/feed.js
+++ public/static/js/apiCalls/feed.js
@@ -3,7 +3,7 @@ import showError from '../utils/showResponseError.js';
 export const getFeedData = async function (id, page) {
   const feedFlow = await axios.get(`/api/v1/feed/project/${id}?limit=10&page=${page}`).catch(showError);
 
-  return feedFlow?.data.data.feed;
+  return feedFlow?.data.data.feeds;
 };
 
 export const createFeed = async function (feedData) {
